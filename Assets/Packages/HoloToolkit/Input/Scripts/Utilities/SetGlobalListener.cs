8835f057b1065b05140dd4eaa2d259ee642e0a0b
Guillaume Nedelec <gnedelec001@IUT.BX1>
2018-07-10 16:52:01 +0200

    jour2

--- Assets/Packages/HoloToolkit/Input/Scripts/Utilities/SetGlobalListener.cs
+++ Assets/Packages/HoloToolkit/Input/Scripts/Utilities/SetGlobalListener.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using UnityEngine;
+using System.Collections;
 
 namespace HoloToolkit.Unity.InputModule
 {
---
+++ Assets/onClickCreateItem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onClickCreateItem : MonoBehaviour {
+
+    public GameObject Prefab;
+    public int RayDistance = 10;
+    private Vector3 Point;
+    public LayerMask Whatever;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    public void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            //Make Whatever a Raycast layer or if you don't want it just exclude it
+            if (Physics.Raycast(ray, out hit, Whatever.value))
+            {
+                Point = hit.point;
+                Instantiate(Prefab, Point, Quaternion.identity);
+            }
+        }
+    }
+}
