394fbe873e510d25d26d2dd2cec5b4ab647a2842
Thomas Blanc <thomas.hakunamacouta.blanc@gmail.com>
2018-07-12 13:12:26 +0200

    add transparency to materials et truc

--- Assets/Scripts/GestureManager.cs
+++ Assets/Scripts/GestureManager.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class GestureManager : MonoBehaviour, IInputClickHandler {
 
 	public Transform cursor;
-	public GameObject platform;
+	public Transform parent;
+	public GameObject[] platforms;
 	public GameObject placeholder;
 	public float cylinderWidth;
 
 	private List<Vector3> points;
 	private Vector3 start;
 	private Vector3 end;
-	private bool firstPointSet = false;
+	private bool firstPointSet;
 	
 	private void Start()
 	{
@@ -24,19 +25,27 @@ public class GestureManager : MonoBehaviour, IInputClickHandler {
 		points.Add(cursor.position);
 		if (!firstPointSet) {
 			start = cursor.position;
-//			placeholder.transform.position = cursor.position;
-//			Instantiate(placeholder);
+			placeholder.transform.position = cursor.position;
+			Instantiate(placeholder);
 			firstPointSet = true;
 		}
 		else {
-//			DestroyImmediate(placeholder, true);
+			placeholder.transform.position = cursor.position;
 			end = cursor.position;
 			Vector3 offset = end - start;
 			Vector3 position = start + offset / 2.0f;
-			var cylinder = Instantiate(platform, position, Quaternion.identity);
+			var cylinder = Instantiate(platforms[0], position, Quaternion.identity, parent);
 			cylinder.transform.up = offset;
 			cylinder.transform.localScale = new Vector3(cylinderWidth, offset.magnitude*3, cylinderWidth);
 			start = end;
 		}
+	}
+
+	public void reset() {
+		firstPointSet = false;
+		points.Clear();
+		start = Vector3.zero;
+		end = Vector3.zero;
+		Destroy(placeholder);
 	}
 }
