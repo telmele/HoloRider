2961cb350e2b0fec6c530910d936caa6e7951a1d
Thomas Blanc <thomas.hakunamacouta.blanc@gmail.com>
2018-07-12 07:15:51 +0200

    push new project

---
+++ Assets/Scripts/GestureManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+public class GestureManager : MonoBehaviour, IInputClickHandler {
+
+	public Transform cursor;
+	public GameObject platform;
+	public GameObject placeholder;
+	public float cylinderWidth;
+
+	private List<Vector3> points;
+	private Vector3 start;
+	private Vector3 end;
+	private bool firstPointSet = false;
+	
+	private void Start()
+	{
+		InputManager.Instance.PushFallbackInputHandler(gameObject);
+		points = new List<Vector3>(); 
+	}
+
+	public virtual void OnInputClicked(InputClickedEventData eventData) {
+		points.Add(cursor.position);
+		if (!firstPointSet) {
+			start = cursor.position;
+//			placeholder.transform.position = cursor.position;
+//			Instantiate(placeholder);
+			firstPointSet = true;
+		}
+		else {
+//			DestroyImmediate(placeholder, true);
+			end = cursor.position;
+			Vector3 offset = end - start;
+			Vector3 position = start + offset / 2.0f;
+			var cylinder = Instantiate(platform, position, Quaternion.identity);
+			cylinder.transform.up = offset;
+			cylinder.transform.localScale = new Vector3(cylinderWidth, offset.magnitude*3, cylinderWidth);
+			start = end;
+		}
+	}
+}
