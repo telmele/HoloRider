70b34b5f61374fd7bd192fb1e348eb10f10a7921
Thomas Blanc <thomas.hakunamacouta.blanc@gmail.com>
2018-07-12 09:56:21 +0200

    Blue/Red/Green pipes and HoloExample

---
+++ Assets/Scripts/AccelerationPipe.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccelerationPipe : MonoBehaviour {
+
+    public float Force;
+    
+    private void OnCollisionStay(Collision other) {
+        Debug.Log("Stay");
+        other.rigidbody.AddForce(Vector3.forward * Force, ForceMode.Acceleration);
+    }
+}
